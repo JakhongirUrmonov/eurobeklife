24e9a0e1b14f5c8bfe7387aac708574d3e6f0c92
Jakhongir-Urmonov <jakhongir.urmonov@qubixstudio.com>
2026-05-17 13:05:53 +0200

    add: added new video and shuffle portfolio

--- src/components/Portfolio.tsx
+++ src/components/Portfolio.tsx
@@ -17,18 +17,19 @@ interface VideoItem {
 }
 
 const portfolioData: VideoItem[] = [
+  { id: "12", titleEn: "Elementa", category: "commercial", titleKey: "Elementa", videoUrl: "https://www.youtube.com/embed/U6e3a6e4wKE", thumbnail: "https://img.youtube.com/vi/U6e3a6e4wKE/maxresdefault.jpg" },
   { id: "01", titleEn: "Apartment · Prague 1", category: "real_estate", titleKey: "Apartment · Prague 1", videoUrl: "https://www.youtube.com/embed/RY-5xUFBwqs", thumbnail: "https://img.youtube.com/vi/RY-5xUFBwqs/maxresdefault.jpg" },
+  { id: "08", titleEn: "DreamFit · Praha", category: "commercial", titleKey: "DreamFit · Praha", videoUrl: "https://www.youtube.com/embed/a0Juwrf7Rk0", thumbnail: "https://img.youtube.com/vi/a0Juwrf7Rk0/maxresdefault.jpg" },
   { id: "02", titleEn: "Apartment · Prague 2", category: "real_estate", titleKey: "Apartment · Prague 2", videoUrl: "https://www.youtube.com/embed/KCh1vBfFyKc", thumbnail: "https://img.youtube.com/vi/KCh1vBfFyKc/maxresdefault.jpg" },
+  { id: "09", titleEn: "GG Barber · Praha", category: "commercial", titleKey: "GG Barber · Praha", videoUrl: "https://www.youtube.com/embed/i6bBTiRBzm0", thumbnail: "https://img.youtube.com/vi/i6bBTiRBzm0/maxresdefault.jpg" },
+  { id: "10", titleEn: "Football Match", category: "events", titleKey: "Football Match", videoUrl: "https://www.youtube.com/embed/phg1i3Wwz6I", thumbnail: "https://img.youtube.com/vi/phg1i3Wwz6I/maxresdefault.jpg" },
   { id: "03", titleEn: "Apartment · Prague 3", category: "real_estate", titleKey: "Apartment · Prague 3", videoUrl: "https://www.youtube.com/embed/xA8fBD5eiRY", thumbnail: "https://img.youtube.com/vi/xA8fBD5eiRY/maxresdefault.jpg" },
+  { id: "11", titleEn: "Barbershop 1", category: "commercial", titleKey: "Barbershop 1", videoUrl: "https://www.youtube.com/embed/4cTA2xZwBiU", thumbnail: "https://img.youtube.com/vi/4cTA2xZwBiU/maxresdefault.jpg" },
   { id: "04", titleEn: "Apartment · Prague 4", category: "real_estate", titleKey: "Apartment · Prague 4", videoUrl: "https://www.youtube.com/embed/i-PiQYXHmwM", thumbnail: "https://img.youtube.com/vi/i-PiQYXHmwM/maxresdefault.jpg" },
+  { id: "13", titleEn: "Barbershop 2", category: "commercial", titleKey: "Barbershop 2", videoUrl: "https://www.youtube.com/embed/rhUIIq4CKwM", thumbnail: "https://img.youtube.com/vi/rhUIIq4CKwM/maxresdefault.jpg" },
   { id: "05", titleEn: "Apartment · Prague 5", category: "real_estate", titleKey: "Apartment · Prague 5", videoUrl: "https://www.youtube.com/embed/PSQejDD7U3E", thumbnail: "https://img.youtube.com/vi/PSQejDD7U3E/maxresdefault.jpg" },
   { id: "06", titleEn: "Apartment · Prague 6", category: "real_estate", titleKey: "Apartment · Prague 6", videoUrl: "https://www.youtube.com/embed/8yCzus2QXOk", thumbnail: "https://img.youtube.com/vi/8yCzus2QXOk/maxresdefault.jpg" },
   { id: "07", titleEn: "Apartment · Prague 7", category: "real_estate", titleKey: "Apartment · Prague 7", videoUrl: "https://www.youtube.com/embed/yAjNFTRjqjw", thumbnail: "https://img.youtube.com/vi/yAjNFTRjqjw/maxresdefault.jpg" },
-  { id: "08", titleEn: "DreamFit · Praha", category: "commercial", titleKey: "DreamFit · Praha", videoUrl: "https://www.youtube.com/embed/a0Juwrf7Rk0", thumbnail: "https://img.youtube.com/vi/a0Juwrf7Rk0/maxresdefault.jpg" },
-  { id: "09", titleEn: "GG Barber · Praha", category: "commercial", titleKey: "GG Barber · Praha", videoUrl: "https://www.youtube.com/embed/i6bBTiRBzm0", thumbnail: "https://img.youtube.com/vi/i6bBTiRBzm0/maxresdefault.jpg" },
-  { id: "10", titleEn: "Football Match", category: "events", titleKey: "Football Match", videoUrl: "https://www.youtube.com/embed/phg1i3Wwz6I", thumbnail: "https://img.youtube.com/vi/phg1i3Wwz6I/maxresdefault.jpg" },
-  { id: "11", titleEn: "Barbershop 1", category: "commercial", titleKey: "Barbershop 1", videoUrl: "https://www.youtube.com/embed/4cTA2xZwBiU", thumbnail: "https://img.youtube.com/vi/4cTA2xZwBiU/maxresdefault.jpg" },
-  { id: "12", titleEn: "Barbershop 2", category: "commercial", titleKey: "Barbershop 2", videoUrl: "https://www.youtube.com/embed/rhUIIq4CKwM", thumbnail: "https://img.youtube.com/vi/rhUIIq4CKwM/maxresdefault.jpg" },
 ];
 
 export default function Portfolio() {
